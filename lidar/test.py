c330bc75d7fc86217182d2b85d6f04837ed17fa0
Sandhiya1405 <148300348+Sandhiya1405@users.noreply.github.com>
2026-05-05 21:28:25 +0530

    Add real-time LiDAR scanning and plotting
    Implement real-time LiDAR data visualization using matplotlib.

--- lidar/test.py
+++ lidar/test.py
@@ -1,1 +1,34 @@
+from rplidar import RPLidar
+import matplotlib.pyplot as plt
+import numpy as np
+
+PORT = 'COM3'
+
+lidar = RPLidar(PORT, baudrate=115200)   # change here
+
+plt.ion()
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='polar')
+
+try:
+    print("Starting LiDAR...")
+
+    for scan in lidar.iter_scans():
+        angles = []
+        distances = []
+
+        for (_, angle, distance) in scan:
+            angles.append(np.deg2rad(angle))
+            distances.append(distance)
+
+        ax.clear()
+        ax.scatter(angles, distances, s=5)
+        plt.pause(0.01)
+
+except KeyboardInterrupt:
+    print("Stopping")
+
+finally:
+    lidar.stop()
+    lidar.disconnect()
 
